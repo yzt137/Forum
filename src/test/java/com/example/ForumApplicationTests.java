1bc6f00f100625352adf32807f0911f05ec3922c
yingzhitao <1942563933@qq.com>
2020-03-12 12:09:54 +0800

    init_2

--- src/test/java/com/example/ForumApplicationTests.java
+++ src/test/java/com/example/ForumApplicationTests.java
@@ -2,15 +2,9 @@ package com.example;
 
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.io.File;
+import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class ForumApplicationTests {
