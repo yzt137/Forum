8d67f8a5eb1bcc44c3b5ddede4393ffcaa0d7b38
yingzhitao_glb <1942563933@qq.com>
2020-03-12 11:56:23 +0800

    init repo

---
+++ src/test/java/com/example/ForumApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
